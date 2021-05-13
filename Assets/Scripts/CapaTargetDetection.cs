829f91c1543da50196c0abd09ad0d8ec71212eb3
MJuniorFBots <mario@facilitabots.com>
2021-05-13 01:35:37 -0300

    Upgrade biblioteca Vuforia 9.8.5 -> 9.8.8
    Implementação cenario da Capa

--- Assets/Scripts/CapaTargetDetection.cs
+++ Assets/Scripts/CapaTargetDetection.cs
@@ -1,65 +1,68 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.Playables;
 using Vuforia;
 
-namespace Assets.Scripts
-{
-    public class CapaTargetDetection : MonoBehaviour
-    {
-        //public new ParticleSystem particleSystem;
+namespace Assets.Scripts {
+    public class CapaTargetDetection : MonoBehaviour {
         private TrackableBehaviour mTrackableBehaviour;
+        public AudioSource audioSource;
         public PlayableDirector director;
+        private IEnumerator fadeInSound, fadeOutSound;
 
         private bool initialPlay = true;
 
-        // Use this for initialization
-        void Start()
-        {
+        void Start() {
             mTrackableBehaviour = GetComponent<TrackableBehaviour>();
-            if (mTrackableBehaviour)
-            {
-                //particleSystem.Stop();
+            fadeInSound = AudioSourceFadeIn(audioSource, .5f);
+            fadeOutSound = AudioSourceFadeOut(audioSource, .1f);
+            if (mTrackableBehaviour) {
                 mTrackableBehaviour.RegisterOnTrackableStatusChanged(OnTrackableStatusChanged);
             }
         }
-        //private void Awake()
-        //{
-        //    if (director != null)
-        //    {
-        //        director.played += Director_Played;
-        //        director.stopped += Director_Stopped;
-        //    }
-        //}
-
-        //private void Director_Stopped(PlayableDirector obj)
-        //{
-        //    controlPanel.SetActive(true);
-        //}
-
-        //private void Director_Played(PlayableDirector obj)
-        //{
-        //    controlPanel.SetActive(false);
-        //}
-
-        void OnTrackableStatusChanged(TrackableBehaviour.StatusChangeResult statusChangeResult)
-        {
+        void OnTrackableStatusChanged(TrackableBehaviour.StatusChangeResult statusChangeResult) {
             if (statusChangeResult.NewStatus == TrackableBehaviour.Status.DETECTED ||
                 statusChangeResult.NewStatus == TrackableBehaviour.Status.TRACKED ||
-                statusChangeResult.NewStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
-            {
-                Debug.Log("OnTrackableStatusChanged: " + statusChangeResult.NewStatus);
+                statusChangeResult.NewStatus == TrackableBehaviour.Status.EXTENDED_TRACKED) {
                 OnTrackingFound();
+            } else {
+                OnTrackingLost();
             }
         }
-        private void OnTrackingFound()
-        {
-            if (/*particleSystem != null && */initialPlay)
-            {
-                //particleSystem.Play();
+        private void OnTrackingFound() {
+            if (initialPlay) {
                 director.Play();
+                audioSource.Play();
+                StopCoroutine(fadeOutSound);
+                StartCoroutine(fadeInSound);
                 initialPlay = false;
             }
         }
+        private void OnTrackingLost() {
+            StopCoroutine(fadeInSound);
+            StartCoroutine(fadeOutSound);
+        }
+
+        public static IEnumerator AudioSourceFadeIn(AudioSource audioSource, float FadeTime) {
+            float startVolume = audioSource.volume;
+
+            while (audioSource.volume < 1) {
+                audioSource.volume += startVolume * Time.deltaTime / FadeTime;
+                yield return null;
+            }
+
+        }
+
+        public static IEnumerator AudioSourceFadeOut(AudioSource audioSource, float FadeTime) {
+            float startVolume = audioSource.volume;
+
+            while (audioSource.volume > 0) {
+                audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+                yield return null;
+            }
+
+        }
+
     }
 
 }
