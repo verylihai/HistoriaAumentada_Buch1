3d12b595cecb3617eec183e0ff5c58dca4d9fd7b
MJuniorFBots <mario@facilitabots.com>
2021-06-06 00:50:08 -0300

    Material refactoring Fairy animation .gitignore

--- Assets/Scripts/CapaFairyPathFollower.cs
+++ Assets/Scripts/CapaFairyPathFollower.cs
@@ -8,17 +8,8 @@ public class CapaFairyPathFollower : MonoBehaviour {
     public bool isPlaying = false;
 
     void Update() {
-        if (isPlaying) {
-            foreach (var item in pathCreator.path.localPoints) {
-                Debug.Log(item);
-            }
-            isPlaying = false;
-        }
         distanceTraveled += speed * Time.deltaTime;
         transform.position = pathCreator.path.GetPointAtDistance(distanceTraveled);
     }
 
-    public Vector3 getTransformPosition() {
-        return transform.position;
-    }
 }
